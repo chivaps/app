82c2c8fee894f652a5a0434fc5a5b54091ea55cf
chivaps <aulol2004@gmail.com>
2024-07-20 03:09:06 +1000

    added private route

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+import { Navigate } from 'react-router-dom';
 import Login from "./components/Login";
 import MainPage from "./components/MainPage";
 import Register from "./components/Register";
 
 function App() {
+  const tokens = localStorage.getItem('jwt');
+  const isAuthenticated = tokens !== null;
 
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login/>} />
-        <Route path="/" element={<MainPage/>} />
+        <Route path="/" element={isAuthenticated ? <MainPage/> : <Navigate to="/login" />} />
         <Route path="/register" element={<Register/>} />
       </Routes>
     </Router>
